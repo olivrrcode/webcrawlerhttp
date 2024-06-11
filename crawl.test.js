039e8e3ad7e708f50f938596ac9a098154443323
OlivrrCode <o.atherton@outlook.com>
2024-06-11 18:08:51 +0100

    normaliseURL tests written

--- crawl.test.js
+++ crawl.test.js
@@ -1,10 +1,34 @@
 const { normaliseURL } = require("./crawl")
 const { test, expect } = require("@jest/globals")
 
-test('normaliseURL', () => {
+test('normaliseURL strip https', () => {
     const input = 'https://blog.boot.dev/path'
     const actual = normaliseURL(input)
-    const expected = ''
+    const expected = 'blog.boot.dev/path'
+
+    expect(actual).toEqual(expected)
+})
+
+test('normaliseURL strip http 2', () => {
+    const input = 'http://blog.boot.dev/path/'
+    const actual = normaliseURL(input)
+    const expected = 'blog.boot.dev/path'
+
+    expect(actual).toEqual(expected)
+})
+
+test('normaliseURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normaliseURL(input)
+    const expected = 'blog.boot.dev/path'
+
+    expect(actual).toEqual(expected)
+})
+
+test('normaliseURL remove capitals', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normaliseURL(input)
+    const expected = 'blog.boot.dev/path'
 
     expect(actual).toEqual(expected)
 })
